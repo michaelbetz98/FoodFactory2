9c7a08ad7ac434283b9191c5db9108d4b9d2148c
michaelbetz <michael.schabetz@stud.tecnicosuperiorekennedy.it>
2024-05-13 14:59:44 +0200

    Add project files.

---
+++ FoodFactory/Exceptions/FoodTypeNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodFactory.Exceptions
+{
+    public class FoodTypeNotFoundException : Exception
+    {
+        public string type { get; set; }
+
+        public FoodTypeNotFoundException(string type)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ FoodFactory/FoodFactory.cs
@@ -0,0 +1,30 @@
+﻿using FoodFactory.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodFactory
+{
+    public class FoodFactory : IFoodFactory
+    {
+        string Name { get; set; }
+        float Price { get; set; }
+        DateTime ExpirationDate { get; set; }
+
+        public FoodFactory NewFood(string type) => type switch
+        {
+            "fruit" => new Fruit(),
+            "meat" => new Meat(),
+            "vegetables" => new Vegetable(),
+            _ => throw new FoodTypeNotFoundException(type)
+        };
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Nome: {this.Name} Scadenza: {this.ExpirationDate} Prezzo: {this.Price}");
+        }
+    }
+}
